6bfdfc7fd3208ef581e290036f2875f287262dd3
Youzi <grapefruit.libre@gmail.com>
2025-04-19 08:01:42 +0800

    all lab01 assignments done

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+
+    if (!head) {
+        return 0;
+    }
+
+    node *hare = head;
+    node *tortoise = head;
+
+    while(hare->next != NULL && hare->next->next != NULL) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
